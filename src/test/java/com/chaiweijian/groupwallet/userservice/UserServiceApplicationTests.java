a16e2e352a1d47ae3510cbf45119495758b8f6c4
Chai Wei Jian <chaiweijian@protonmail.com>
2021-08-18 21:09:12 +0800

    Initial Commit

---
+++ src/test/java/com/chaiweijian/groupwallet/userservice/UserServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chaiweijian.groupwallet.userservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
